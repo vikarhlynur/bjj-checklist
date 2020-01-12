b53fea72c6059bee12cde1cf50e7a5d2133c67ca
Jafet Bjarkar <jafetbjarkar@gmail.com>
2020-01-12 22:36:58 +0000

    Fix environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAsQ5B4Iwx33dD7dkWtsKFODg1PFq3N8FE',
+    authDomain: 'bjj-checklist.firebaseapp.com',
+    databaseURL: 'https://bjj-checklist.firebaseio.com',
+    projectId: 'bjj-checklist',
+    storageBucket: 'bjj-checklist.appspot.com',
+    messagingSenderId: '61064099993',
+    appId: '1:61064099993:web:cb945d20ba742a6c0d2008',
+    measurementId: 'G-G4R6K9TF1Y'
+  }
 };
