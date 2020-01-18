ab04909af03b43a317eb88259d0a2dbb65e8436a
Jafet Bjarkar <jafetbjarkar@gmail.com>
2020-01-18 18:27:01 +0000

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,9 @@ import { BjjChecklistComponent } from './bjj-checklist/bjj-checklist.component';
 import { BjjChecklistDataComponent } from './bjj-checklist-data/bjj-checklist-data.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'bjj-checklist', pathMatch: 'full' },
-  { path: 'bjj-checklist', component: BjjChecklistComponent },
-  { path: 'bjj-checklist-data', component: BjjChecklistDataComponent },
-  { path: '**', redirectTo: 'bjj-checklist' }
+  { path: '', component: BjjChecklistComponent, pathMatch: 'full' },
+  { path: 'data', component: BjjChecklistDataComponent },
+  { path: '**', component: BjjChecklistComponent }
 ];
 
 @NgModule({
