71945625e3623e387300668af52e4ab227c1aa36
Jafet Bjarkar <jafetbjarkar@gmail.com>
2020-01-30 21:11:15 +0000

    fix: missing start time

--- src/app/bjj-checklist/models/technique.model.ts
+++ src/app/bjj-checklist/models/technique.model.ts
@@ -118,7 +118,7 @@ export class TechniqueVideo {
     if (!video || !video[0] || !Array.isArray(video)) { return; }
     this.id = video[0];
     this.start = video[1] !== undefined ? video[1] : 0;
-    this.url = `https://www.youtube.com/embed/${this.id}`;
+    this.url = `https://www.youtube.com/embed/${this.id}?start=${this.start}`;
   }
 }
 
