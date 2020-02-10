309eb3821b372baf0478070e2983673bd0008a99
Jafet Bjarkar <jafetbjarkar@gmail.com>
2020-02-10 23:19:24 +0000

    style: hide belt caption on small screens

--- src/app/checklist/list/checklist-list.component.ts
+++ src/app/checklist/list/checklist-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, HostListener } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { get, sortBy } from 'lodash';
 
